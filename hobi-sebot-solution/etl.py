9c93519cac20b6931ca99839b4b5400d5a4680fa
papsebestyen <pap.sebestyen@gmail.com>
2022-03-12 20:41:46 +0100

    drop duplicates

--- hobi-sebot-solution/etl.py
+++ hobi-sebot-solution/etl.py
@@ -3,7 +3,15 @@ import pickle
 import pandas as pd
 from constants import tree_path, data_path
 
-df = pd.read_csv("data.csv")
+
+pos_cols = [f"{ax}_position" for ax in ["x", "y", "z"]]
+
+df = (
+    pd.read_csv("data.csv")
+    .drop_duplicates(pos_cols)
+    .dropna()
+    .loc[:, lambda _df: _df.nunique() != 1]
+)
 tree = cKDTree(df.loc[:, df.columns.str.endswith("position")].values)
 
 tree_path.write_bytes(pickle.dumps(tree))
